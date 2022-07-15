58acd0bd54c0fb2ff46ed4c04f50a36b05b1ee50
rahulvk007 <rahulvijayakumarpt4551@gmail.com>
2022-07-15 09:34:33 +0530

    added .gitignore

--- services/admin.py
+++ services/admin.py
@@ -2,7 +2,6 @@ from .models import *
 from django.contrib import admin
 from .search import *
 
-# Register your models here.
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Work)
 admin.site.register(Progress)
